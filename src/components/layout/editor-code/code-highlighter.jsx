3b8afb1fb51698ffabc0864bbecd726e3e47b48f
kushinar09 <phongpd109@gmail.com>
2025-03-31 17:51:19 +0700

    .

--- src/components/layout/editor-code/code-highlighter.jsx
+++ src/components/layout/editor-code/code-highlighter.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react"
 import { cn } from "@/lib/utils"
 import Prism from "prismjs"
 import "prismjs/components/prism-core"
+import "prismjs/components/prism-clike"
 import "prismjs/components/prism-javascript"
 import "prismjs/plugins/line-numbers/prism-line-numbers"
 import "./css/prism-darcula.css"
@@ -16,8 +17,8 @@ export function CodeHighlighter({ code, language = "javascript", showLineNumbers
     // Highlight the code
     const highlighted = Prism.highlight(
       code,
-      Prism.languages[language] || Prism.languages.javascript,
-      language,
+      Prism.languages[language.toLowerCase()] || Prism.languages.javascript,
+      language.toLowerCase()
     )
     setHighlightedCode(highlighted)
   }, [code, language])
